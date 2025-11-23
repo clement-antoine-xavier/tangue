97f6fd1acc9b1d4d94f5d88e35c5ba8f1f85de87
Clément Antoine XAVIER <clement-antoine.xavier@epitech.eu>
2025-11-23 14:32:23 +0900

    Feat: Add documentation with Doxygen and it workflow

--- headers/application.hpp
+++ headers/application.hpp
@@ -2,10 +2,20 @@
 
 namespace tangue
 {
+    /**
+     * @brief The Application class represents the main application.
+     */
     class Application
     {
     public:
+        /**
+         * @brief Default constructor for the Application class.
+         */
         Application(void);
+
+        /**
+         * @brief Destructor for the Application class.
+         */
         ~Application(void);
     };
 } // namespace tangue
